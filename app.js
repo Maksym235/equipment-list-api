bbf23ff6d659af0270bcd01634ac6c934a6af3fa
Maksym235 <andriichukwork235@gmail.com>
2023-08-21 21:39:48 +0300

    fix deploy vercel 2

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
+const mongoose = require("mongoose");
+const { DB_HOST } = process.env;
 const personsRouter = require("./routes/persons");
 const equipRouter = require("./routes/equip");
 const usersRouter = require("./routes/auth");
@@ -25,5 +27,21 @@ app.use((req, res) => {
 app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
+
+
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000, console.log("Server running. Use our API on port: 3000"));
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
+
 
 module.exports = app;
