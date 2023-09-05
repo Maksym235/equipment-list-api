38850eceac2c52951fd1875182e75c9ea05aa2dd
Maks <109439302+Maksym235@users.noreply.github.com>
2023-09-05 09:56:51 +0000

    fix vercel

--- api/index.js
+++ api/index.js
@@ -1,6 +1,41 @@
-const app = require('./index')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+const http = require("http");
+const mongoose = require("mongoose");
+require("dotenv").config();
 const mongoose = require("mongoose");
 const { DB_HOST } = process.env;
+const personsRouter = require("./routes/persons");
+const equipRouter = require("./routes/equip");
+const equipsListRouter = require("./routes/equipsList");
+const usersRouter = require("./routes/auth");
+const toursRouter = require("./routes/tours");
+const app = express();
+mongoose.set("strictQuery", true);
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/persons", personsRouter);
+app.use("/equip", equipRouter);
+app.use("/equipsList", equipsListRouter);
+app.use("/auth", usersRouter);
+app.use("/tours", toursRouter);
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message });
+});
+
 
 mongoose.set("strictQuery", true);
 
@@ -14,3 +49,5 @@ mongoose
     console.log(err.message);
     process.exit(1);
   });
+
+module.exports = app;
